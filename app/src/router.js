8c5a53374b9cd040a1b65dcce60da3d3dc5b09d6
Robyn Navarro <rnavarro182@gmail.com>
2018-06-11 14:53:04 -0700

    components routed

--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,28 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
 import About from './components/About.vue';
+import Albums from './components/Albums.vue';
+import AlbumDetail from './components/AlbumDetail.vue';
+import ThumbnailViewer from './components/ThumbnailViewer.vue';
+import GalleryViewer from './components/GalleryViewer.vue';
+import ListViewer from './components/ListViewer.vue';
+import NewImage from './components/NewImage.vue';
+
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
-    { path: '/about', component: About }
+    { path: '/about', component: About },
+    { path: '/albums', component: Albums },
+    {
+      path: '/albums/:id',
+      component: AlbumDetail,
+      children: [
+        { path: 'thumbnail', component: ThumbnailViewer },
+        { path: 'gallery', component: GalleryViewer },
+        { path: 'list', component: ListViewer },
+        { path: 'new', component: NewImage }
+      ]
+    }
   ]
 });
