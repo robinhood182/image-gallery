893501d2b81a7b6abc85441db31e18e46abb6fca
Arthur Jen <arthurljen@gmail.com>
2018-06-11 15:20:38 -0700

    components creatd

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <h1>Image Gallery</h1>
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/albums">Albums</router-link>
     </nav>
     <router-view/>
   </div>
---
+++ app/src/components/AddAlbum.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Add Album</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
+
