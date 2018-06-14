b84ecc5be99d65e8339a99886ff3136e673d969a
Robyn Navarro <rnavarro182@gmail.com>
2018-06-13 17:57:14 -0700

    error handling and some sweet transition in progress

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="home">
+    
     <img src="https://i.imgur.com/l0t8axs.jpg">
     <section class="home-text">
       <h1>Commemorate the ones you love</h1>
       <router-link to="/albums/new"><button>Get Started</button></router-link>
     </section>
-    <ul v-if="stats">
-      <li>Total albums: {{stats.count}}</li>
-      <li>Average memories per album: {{parseInt(stats.avg)}}</li>
-      <li>Least amount of memories per album: {{stats.min}}</li>
-      <li>Most amount of memories per album: {{stats.max}}</li>
-    </ul>
+  
+    <div class="stats" @click="shown = !shown">
+        <h3 >Stats</h3>
+      <transition name="slide">
+        <section v-if="shown">
+          <ul>
+            <li>Total albums: {{stats.count}}</li>
+            <li>Average memories per album: {{parseInt(stats.avg)}}</li>
+            <li>Least amount of memories per album: {{stats.min}}</li>
+            <li>Most amount of memories per album: {{stats.max}}</li>
+          </ul>
+        </section>
+      </transition>
+    </div>
+
   </div>
 </template>
 
@@ -21,6 +31,7 @@ export default {
 
   data() {
     return {
+      shown: false,
       stats: null
     };
   },
@@ -36,9 +47,11 @@ export default {
 
 <style scoped>
 #home {
-  padding: 100px 0px;
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template: 300px 300px / 1fr 1fr;
+  grid-template-areas:
+    "picture words"
+    "stats stats"
 }
 
 h1 {
@@ -46,12 +59,37 @@ h1 {
 }
 
 .home-text {
+  grid-area: words;
   text-align: center;
 }
 
 img {
+  grid-area: picture;
   width: 40%;
   border-radius: 25px;
+}
+
+li {
+  list-style: none;
+}
+
+.stats {
+  grid-area: stats;
+  background-color: rgb(88, 88, 88);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.slide-enter-active {
+  transition: all .7s ease;
+}
+.slide-leave-active {
+  transition: all .4s ease;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateY(50px);
 }
 </style>
 
